583b016a49a796c80e841f090641997eb2cac92a
Monkeyyy11 <gaileshd@gmail.com>
2019-05-31 14:59:22 +0200

    blogicon and blogname can now be updated

--- build.js
+++ build.js
@@ -1,10 +1,56 @@
 #! /usr/bin/env node
 
 const program = require('commander');
+const version = require('./package.json').version;
+const fs = require('fs');
 
 program
-    .option('-n, --name {name of your blog}', 'Name of your blog')
+    .version(version)
+    .option('-n, --blogname {name of your blog}', 'Name of your blog')
+    .option('-i, --blogicon <url>', 'Icon of your blog')
+    .option('-u, --update', 'Updates your blog website with the newest blog entries')
+    .option('-c, --create <title>', 'Creates a new blog entry')
+    .option('-b, --background <url>', 'Background image')
     .parse(process.argv);
 
+if (program.blogname) {
+    var settingsJson = JSON.parse(fs.readFileSync('settings.json', err => {
+        if (err) {
+            return console.error('Couldn\'t load settings.json!')
+        } else {
+            console.log('Loaded settings.json!')
+        }
+    }).toString());  
 
-console.log(1, program)
+    settingsJson[0].blogName = program.args[0];
+
+    return fs.writeFile('settings.json', JSON.stringify(settingsJson, null, 2), err => {
+        if (err) {
+            console.error('Couldn\'t update settings.json!');
+        } else {
+            console.log('Updated settings.json!')
+        }
+    })
+}
+
+if (program.blogicon) {
+    var settingsJson = JSON.parse(fs.readFileSync('settings.json', err => {
+        if (err) {
+            return console.error('Couldn\'t load settings.json!')
+        } else {
+            console.log('Loaded settings.json!')
+        }
+    }).toString());
+
+    if (program.args[0].match(/\.(jpeg|jpg|png)$/) === null) return console.error('Your image link has to end with .JPEG, .JPG or .PNG!')
+
+    settingsJson[0].blogIcon = program.args[0];
+
+    return fs.writeFile('settings.json', JSON.stringify(settingsJson, null, 2), err => {
+        if (err) {
+            console.error('Couldn\'t update settings.json!');
+        } else {
+            console.log('Updated settings.json!')
+        }
+    })
+}
