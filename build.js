6c677fb3b4c8b7f5ea610777bf8ab83c2c1ab144
Monkeyyy11 <gaileshd@gmail.com>
2019-06-01 09:39:47 +0200

    ficxes

--- build.js
+++ build.js
@@ -13,11 +13,11 @@ program
 	.option('-n, --blogname {name of your blog}', 'Name of your blog')
 	.option('-i, --blogicon <url>', 'Icon of your blog')
 	.option('-u, --update', 'Updates your blog website with the newest blog entries')
-	.parse(process.argv);
+    .parse(process.argv);
+    
+    const settingsJson = JSON.parse(fs.readFileSync('settings.json').toString());
 
 if (program.blogname) {
-	const settingsJson = JSON.parse(fs.readFileSync('settings.json').toString());
-
 	settingsJson[0].blogName = program.args[0];
 
 	fs.writeFile('settings.json', JSON.stringify(settingsJson, null, 2), err => {
@@ -30,8 +30,6 @@ if (program.blogname) {
 }
 
 if (program.blogicon) {
-	const settingsJson = JSON.parse(fs.readFileSync('settings.json').toString());
-
 	if (program.args[0].match(/\.(jpeg|jpg|png)$/) === null) return console.error('Your image link has to end with .JPEG, .JPG or .PNG!');
 
 	settingsJson[0].blogIcon = program.args[0];
@@ -56,9 +54,9 @@ fs.copyFile('./assets/blogTemplate.html', `./blog/index.html`, err => {
 		style.setAttribute('href', './index.css');
 		document.getElementsByTagName('head')[0].appendChild(style);
 
-		document.getElementsByTagName('title')[0].textContent = program.blogname ? program.blogname : 'Name of the Blog';
-		document.getElementById('blog_title').textContent = program.blogname ? program.blogname : 'Name of the Blog';
-		document.getElementById('blogIcon').setAttribute('href', program.blogicon ? program.blogicon : 'https://i.imgur.com/TFle1Cb.png');
+		document.getElementsByTagName('title')[0].textContent = `${settingsJson[0].blogName}'s Blog` ? `${settingsJson[0].blogName}'s Blog` : 'Name of the Blog';
+		document.getElementById('blog_title').textContent = `${settingsJson[0].blogName}'s Blog` ? `${settingsJson[0].blogName}'s Blog` : 'Name of the Blog';
+		document.getElementById('blogIcon').setAttribute('href', settingsJson[0].blogIcon ? settingsJson[0].blogIcon : 'https://i.imgur.com/TFle1Cb.png');
 
 		fs.writeFile(`./blog/index.html`, `<!DOCTYPE html>${window.document.documentElement.outerHTML}`, error => {
 			if (error) throw error;
